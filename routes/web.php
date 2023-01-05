77d94dce275ee07675d4a7d5e3b9639f35dffc79
吴文健 <472372045@qq.com>
2023-01-05 15:34:27 +0800

    更新目录结构

--- routes/web.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use afunjoker\LaravelAdminPlupload\Http\Controllers\LaravelAdminPluploadController;
-
-Route::post('{customer}/{project}/{model}/laravel-admin-plupload/upload', LaravelAdminPluploadController::class.'@upload');
